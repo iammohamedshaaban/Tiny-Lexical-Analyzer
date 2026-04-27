5c34450af49b76c6977f17f70666428b537bba35
Mohamed Shaaban <moshaaban2004@gmail.com>
2026-04-27 20:26:38 +0300

    Initial commit with lexer and parser

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputString = textBox1.Text;
+
+            string keywords = @"\b(num|text|check|otherwise|until|repeat|then)\b";
+            string identifiers = @"\b[a-zA-Z][a-zA-Z0-9]*\b";
+            string numbers = @"\b[0-9]+\b";
+            string operators = @":=|==|[+\-*/<>]";
+            string symbols = @"[;{}()]";
+            string masterPattern = $"{keywords}|{numbers}|{operators}|{symbols}|{identifiers}";
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("lexeme");
+            dt.Columns.Add("Token type");
+
+            MatchCollection matches = Regex.Matches(inputString, masterPattern);
+            List<Token> allTokens = new List<Token>();
+            foreach (Match m in matches)
+            {
+
+                string lex = m.Value.Trim();
+                string type = "";
+
+                if (Regex.IsMatch(lex, keywords))
+                    type = "keyword";
+                else if (Regex.IsMatch(lex, numbers))
+                    type = "number";
+                else if (Regex.IsMatch(lex, identifiers))
+                    type = "Identifier";
+                else if (lex == ":=") type = "Assignment_Op";
+                else if (lex == "==") type = "Equal_Op";
+                else if (lex == "+") type = "Plus_Op";
+                else if (lex == "-") type = "Minus_Op";
+                else if (lex == "*") type = "Multiply_Op";
+                else if (lex == "/") type = "Divide_Op";
+                else if (lex == ">") type = "Greater_Than_Op";
+                else if (lex == "<") type = "Less_Than_Op";
+
+                else if (lex == ";") type = "Semicolon";
+                else if (lex == "(") type = "Left_Paren";
+                else if (lex == ")") type = "Right_Paren";
+                else if (lex == "{") type = "Left_Brace";
+                else if (lex == "}") type = "Right_Brace";
+                else type = "unknown";
+
+                allTokens.Add(new Token { Value = lex, Type = type });
+                dt.Rows.Add(lex, type);
+            }
+            dataGridView1.DataSource = dt;
+
+            try
+            {
+                if (allTokens.Count == 0) return;
+                TinyParser parser = new TinyParser(allTokens);
+                parser.ParseProgram();
+                MessageBox.Show("success: Your Tiny code is correct!", "success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($" Syntax Error: {ex.Message}", "Parsing Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+            private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/TinyParser.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class TinyParser
+    {
+        private List<Token> tokens;
+        private int index = 0;
+        private Token currentToken;
+
+        public TinyParser(List<Token> tokens)
+        {
+            this.tokens = tokens;
+            if (tokens.Count > 0)
+                currentToken = tokens[index];
+        }
+        private void Match(string expected)
+        {
+            if (index < tokens.Count && (currentToken.Value == expected || currentToken.Type == expected))
+            {
+                index++;
+                if (index < tokens.Count)
+                    currentToken = tokens[index];
+
+            }
+            else
+            {
+                throw new Exception($"Syntax error: expected {expected} but found {currentToken?.Type}");
+            }
+        }
+        public void ParseProgram()
+        {
+            ParseStatements();
+        }
+        private void ParseStatements()
+        {
+            ParseStatement();
+            if (index < tokens.Count && currentToken.Value == ";")
+            {
+                Match(";");
+                if (index < tokens.Count && currentToken.Value != "}" && currentToken.Value != ";")
+                {
+                    ParseStatements();
+                }
+            }
+        }
+        private void ParseStatement()
+        {
+            if (index >= tokens.Count) return;
+            if (currentToken.Value == "num" || currentToken.Value == "text")
+            {
+                ParseDecleration();
+            }
+            else if (currentToken.Type == "Identifier")
+            {
+                ParseAssignment();
+            }
+            else if (currentToken.Value == "check")
+            {
+                ParseCheck();
+            }
+            else if (currentToken.Value == "repeat")
+            {
+                ParseRepeat();
+            }
+            else if (currentToken.Value == "{")
+            {
+                ParseBlock();
+            }
+            else
+            {
+                throw new Exception($"Syntax error: unexpected token {currentToken.Value}");
+            }
+        }
+        private void ParseDecleration()
+        {
+            if (currentToken.Value == "num") Match("num");
+            else Match("text");
+
+            ParseAssignment();
+
+        }
+        private void ParseAssignment()
+        {
+            if (currentToken.Type != "Identifier")
+            {
+                throw new Exception($"Syntax error: expected identifier but found {currentToken.Type}");
+            }
+            Match("Identifier");
+            Match(":=");
+            ParseExpression();
+        }
+        private void ParseCheck()
+        {
+            Match("check");
+            Match("(");
+            ParseCondition();
+            Match(")");
+            ParseBlock();
+
+            if (index < tokens.Count && currentToken.Value == "otherwise")
+            {
+                Match("otherwise");
+                ParseBlock();
+            }
+        }
+        private void ParseRepeat()
+        {
+            Match("repeat");
+            ParseBlock();
+            Match("until");
+            Match("(");
+            ParseCondition();
+            Match(")");
+        }
+        private void ParseBlock()
+        {
+            if (currentToken.Value == "{")
+            {
+                Match("{");
+                ParseStatements();
+                Match("}");
+            }
+            else
+            {
+                ParseStatement();
+            }
+        }
+        private void ParseCondition()
+        {
+            ParseExpression();
+            if (currentToken.Value == "<" || currentToken.Value == ">" || currentToken.Value == "==")
+                Match(currentToken.Value);
+            else
+                throw new Exception("expected Relation operator (<,>,==<!=)");
+            ParseExpression();     
+        }
+        private void ParseExpression()
+        {
+            ParseTerm();
+            while (index < tokens.Count && (currentToken.Value == "+" || currentToken.Value == "-"))
+            {
+                Match(currentToken.Value);
+                ParseTerm();
+            }
+        }
+        private void ParseTerm()
+        {
+            ParseFactor();
+            while (index < tokens.Count && (currentToken.Value == "*" || currentToken.Value == "/"))
+            {
+                Match(currentToken.Value);
+                ParseFactor();
+            }
+        }
+        private void ParseFactor()
+        {
+            if (currentToken.Type == "Identifier") Match("Identifier");
+            else if (currentToken.Type == "number") Match("number");
+            else if (currentToken.Value == "(")
+            {
+                Match("(");
+                ParseExpression();
+                Match(")");
+            }
+            else
+            {
+                throw new Exception($"Syntax error: unexpected token {currentToken.Value}");
+            }
+        }
+
+    } 
+}
